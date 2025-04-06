39035e541a204bdfe1f8cefd72bbd1adcb8e444f
chandra prakash <prakash39911@gmail.com>
2025-04-07 00:14:29 +0530

    slight improvement in UI

--- src/components/VideoPlayerSideBar.tsx
+++ src/components/VideoPlayerSideBar.tsx
@@ -42,12 +42,14 @@ export default function VideoPlayerSideBar({
     <div
       className={clsx(
         "bg-gray-900 text-white transition-all duration-300 ease-in-out flex flex-col vertical-center overflow-y-auto",
-        isCollapsed ? "w-14" : "w-96"
+        isCollapsed ? "w-10 md:w-14" : "w-44 md:w-80"
       )}
     >
-      <div className="flex justify-end">
+      <div className={`flex ${isCollapsed ? "justify-center" : "justify-end"}`}>
         <button
-          className="flex mt-3 mr-3 w-8 h-8 bg-gray-700 rounded-lg text-gray-300"
+          className={`flex mt-3 ${
+            isCollapsed ? "" : "mr-2"
+          } w-8 h-8 bg-gray-700 rounded-lg text-gray-300`}
           onClick={() => setisCollapsed((prevState: any) => !prevState)}
         >
           {isCollapsed ? <ChevronRight size={30} /> : <ChevronLeft size={30} />}
@@ -56,7 +58,7 @@ export default function VideoPlayerSideBar({
 
       <div>
         {!isCollapsed && (
-          <div className="text-2xl text-gray-400 font-bold border-t border-gray-400 border-b mt-2 p-1.5 mb-3">
+          <div className="text-xl md:text-2xl text-gray-400 font-bold border-t border-gray-400 border-b mt-2 p-1.5 mb-3">
             Course: {course.title}
           </div>
         )}
@@ -69,11 +71,11 @@ export default function VideoPlayerSideBar({
               key={eachSection.id}
               className="bg-gray-700 mb-2 rounded-md p-2 m-2 border border-gray-600 bg-transparent hover:bg-transparent/50"
             >
-              <div className="flex gap-2 items-center">
-                <div className="text-xl text-gray-400">
+              <div className="flex flex-wrap gap-2 items-center">
+                <div className="text-sm md:text-xl text-gray-400">
                   Section : {index + 1}
                 </div>
-                <div className="font-bold text-xl text-gray-200">
+                <div className="font-bold text-sm md:text-xl text-gray-200">
                   {eachSection.sectionName}
                 </div>
               </div>
@@ -82,22 +84,22 @@ export default function VideoPlayerSideBar({
                   <div
                     key={eachVideo.id}
                     className={clsx(
-                      "mb-1 bg-gray-800 mt-5 p-2 rounded-lg h-[46px]",
+                      "mb-1 bg-gray-800 mt-2 md:mt-4 p-1 md:p-2 rounded-lg h-[30px] md:h-[46px]",
                       eachVideo.id === currentVideo.id
                         ? "border-2 border-blue-800"
                         : ""
                     )}
                     onClick={() => handleVideoClick(eachVideo.id)}
                   >
-                    <div className="flex hover:font-semibold text-xl justify-between items-center text-gray-200 cursor-pointer">
+                    <div className="flex hover:font-semibold text-sm md:text-xl justify-between items-center text-gray-200 cursor-pointer">
                       <div className="flex gap-1 items-center">
-                        <Video />
+                        <Video size={16} />
                         <div>{eachVideo.video_title}</div>
                       </div>
                       <div>
                         {eachVideo.id === currentVideo.id ? (
                           <div className="transition-all animate-pulse text-gray-300">
-                            <Play size={22} />
+                            <Play size={18} />
                           </div>
                         ) : (
                           ""
